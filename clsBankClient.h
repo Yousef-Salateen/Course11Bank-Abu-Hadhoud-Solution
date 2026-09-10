287f6fd3d611cbca964fda9ce50e8e423b05fbf6
Yousef <yousef.moh.salateen@gmail.com>
2026-09-11 00:59:44 +0300

    Created client class with constructor, setters, and getters

--- clsBankClient.h
+++ clsBankClient.h
@@ -1,8 +1,55 @@
 #pragma once
+
+#include <iostream>
+#include <string>
+#include<vector>
 #include "clsPerson.h"
+#include "clsString.h"
+#include <fstream>
+
 class clsBankClient :
     public clsPerson
 {
+private:
+    enum enMode { _EmptyMode = 0, _UpdateMode };
+    enMode _Mode;
+
+    std::string _AccNumber;
+    std::string _PinCode;
+    double _Balance;
+
+
+public:
+    clsBankClient(const std::string& FirstName, const std::string& LastName, const std::string& Email, const std::string& Phone,
+        const std::string& AccNumber, const std::string& PinCode, double Balance, enMode Mode)
+        : clsPerson(FirstName, LastName, Email, Phone), _AccNumber(AccNumber), _PinCode(PinCode), _Balance(Balance), _Mode(Mode)
+    {
+
+    }
+
+    void setPinCode(const std::string& PinCode)
+    {
+        _PinCode = PinCode;
+    }
+
+    void setBalance(double Balance)
+    {
+        _Balance = Balance;
+    }
+
+    std::string AccNumber()
+    {
+        return _AccNumber;
+    }
+
+    std::string PinCode()
+    {
+        return _PinCode;
+    }
+
+    double Balance()
+    {
+        return _Balance;
+    }
 
 };
-
