778b56fcf279adb9ff3606ebff84d83bbae92bf4
Yousef <yousef.moh.salateen@gmail.com>
2026-09-11 00:36:41 +0300

    Created person class with setters and getters

--- clsPerson.h
+++ clsPerson.h
@@ -1,6 +1,67 @@
 #pragma once
+
+#include <iostream>
+#include <string>
+
 class clsPerson
 {
+private:
+	std::string _FirstName;
+	std::string _LastName;
+	std::string _Email;
+	std::string _Phone;
 
+public:
+
+	clsPerson(const std::string& FirstName, const std::string& LastName, const std::string& Email, const std::string& Phone) :
+		_FirstName(FirstName), _LastName(LastName), _Email(Email), _Phone(Phone)
+	{
+
+	}
+
+	void setFirstName(const std::string& FirstName)
+	{
+		_FirstName = FirstName;
+	}
+
+	void setLastName(const std::string& LastName)
+	{
+		_LastName = LastName;
+	}
+
+	void setEmail(const std::string& Email)
+	{
+		_Email = Email;
+	}
+
+	void setPhone(const std::string& Phone)
+	{
+		_Phone = Phone;
+	}
+
+	std::string FirstName()
+	{
+		return _FirstName;
+	}
+
+	std::string LastName()
+	{
+		return _LastName;
+	}
+
+	std::string FullName()
+	{
+		return _FirstName + " " + _LastName;
+	}
+
+	std::string Email()
+	{
+		return _Email;
+	}
+
+	std::string Phone()
+	{
+		return _Phone;
+	}
 };
 
